d1e138145ad0cb84efcf7161b21857358b451737
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2024-04-28 11:31:55 +0530

    change btn color

--- script.js
+++ script.js
@@ -8,12 +8,13 @@ const soundbtn = document.querySelector(".sound");
 form.addEventListener("submit", WordMeaning);
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en";
 
+
 // if(input.value.match(/^[A-Za-z]+$/)){
     async function WordMeaning(e) {
         e.preventDefault();
         try {
             const WordData = await fetch(`${url}/${input.value}`);
-        const wordObj = await WordData.json();
+            const wordObj = await WordData.json();
         WordHeading.innerHTML = (wordObj[0].word).toLocaleLowerCase()
         Definitions.innerHTML = (wordObj[0].meanings[0].definitions[0].definition || "error");
         example.classList.add("example");
@@ -23,17 +24,13 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en";
         soundbtn.innerHTML = (`<i class="fa-solid fa-volume-high"></i>`);
         let speech = new SpeechSynthesisUtterance();
         soundbtn.addEventListener("click", () => {
-            // speech.text = document.querySelector("WordHeading").value;
             speech.text = WordHeading.innerHTML;  
-            // speech.text =  Definitions.innerHTML;  
             window.speechSynthesis.speak(speech);
         });
         } catch (error) {
             WordHeading.innerHTML = "I don't know"
+            wordObj;
         }
     }
-// }else{
-//     prompt("hey")
-// }
 
 
